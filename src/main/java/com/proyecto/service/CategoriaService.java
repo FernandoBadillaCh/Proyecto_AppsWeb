e8c353215b30ac2735943081329a2a41ca82b093
Fernando Badilla Chinchilla <133604779+FernandoBadillaCh@users.noreply.github.com>
2024-07-22 21:28:25 -0600

    CRUD Producto y Categoria completo

--- src/main/java/com/proyecto/service/CategoriaService.java
+++ src/main/java/com/proyecto/service/CategoriaService.java
@@ -1,6 +1,8 @@
 package com.proyecto.service;
 
 import com.proyecto.domain.Categoria;
+import com.proyecto.domain.Producto;
+
 import java.util.List;
 
 public interface CategoriaService {
@@ -8,4 +10,8 @@ public interface CategoriaService {
 	public List<Categoria> getCategorias();
 
 	public Categoria getCategoria(Long idCategoria);
+
+	public Categoria delete(Long idProducto);
+
+	public void guardar(Categoria categoria);
 }
