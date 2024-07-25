c3c19929a09ad12815636ad934f7b61a66d94c03
Javieraj2715 <pepon.012728@gmail.com>
2024-07-25 17:29:15 -0600

    Eliminacion Clases
    Se eliminaron las clases de Plato fuerte, postre, bebida y entrada

--- src/main/java/com/proyecto/dao/BebidaDao.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.dao;
-
-import com.proyecto.domain.Bebida;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BebidaDao extends JpaRepository <Bebida,Long> {
-    
-}
--- src/main/java/com/proyecto/dao/PostreDao.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.dao;
-
-import com.proyecto.domain.Postre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostreDao extends JpaRepository <Postre,Long> {
-    
-}
